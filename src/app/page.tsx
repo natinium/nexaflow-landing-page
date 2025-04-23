b68486b1ab1508e167e3168a98c1f192b19bc641
Natnael Mesele <nmesele5@gmail.com>
2025-04-23 14:42:14 +0300

    ⚡️ perf(project): use next/dynamic to lazy load sections to improve performance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,43 @@
 import ThemeToggle from '@/components/ThemeToggle/component';
+import dynamic from 'next/dynamic';
 
-import {
-  HeaderSection,
-  HeroSection,
-  TrustedBySection,
-  BenefitsSection,
-  FeatureSection1,
-  FeatureSection2,
-  FeatureSection3,
-  FeatureSection4,
-  StatsSection,
-  TestimonialSection,
-  FaqSection,
-  ContactSection,
-  PricingSection,
-  CTASection,
-  FooterSection,
-} from '@/components/sections';
+// Directly import components likely to be above the fold
+import { HeaderSection, HeroSection } from '@/components/sections';
+
+// Dynamically import components likely to be below the fold
+const TrustedBySection = dynamic(() =>
+  import('@/components/sections').then((mod) => mod.TrustedBySection),
+);
+const BenefitsSection = dynamic(() =>
+  import('@/components/sections').then((mod) => mod.BenefitsSection),
+);
+const FeatureSection1 = dynamic(() =>
+  import('@/components/sections').then((mod) => mod.FeatureSection1),
+);
+const FeatureSection2 = dynamic(() =>
+  import('@/components/sections').then((mod) => mod.FeatureSection2),
+);
+const FeatureSection3 = dynamic(() =>
+  import('@/components/sections').then((mod) => mod.FeatureSection3),
+);
+const FeatureSection4 = dynamic(() =>
+  import('@/components/sections').then((mod) => mod.FeatureSection4),
+);
+const StatsSection = dynamic(() => import('@/components/sections').then((mod) => mod.StatsSection));
+const TestimonialSection = dynamic(() =>
+  import('@/components/sections').then((mod) => mod.TestimonialSection),
+);
+const FaqSection = dynamic(() => import('@/components/sections').then((mod) => mod.FaqSection));
+const ContactSection = dynamic(() =>
+  import('@/components/sections').then((mod) => mod.ContactSection),
+);
+const PricingSection = dynamic(() =>
+  import('@/components/sections').then((mod) => mod.PricingSection),
+);
+const CTASection = dynamic(() => import('@/components/sections').then((mod) => mod.CTASection));
+const FooterSection = dynamic(() =>
+  import('@/components/sections').then((mod) => mod.FooterSection),
+);
 
 const Home = () => {
   return (
@@ -24,8 +45,11 @@ const Home = () => {
       <div className="fixed top-10 right-10 z-51">
         <ThemeToggle />
       </div>
+      {/* Components likely above the fold */}
       <HeaderSection />
       <HeroSection />
+
+      {/* Components likely below the fold (lazy loaded) */}
       <TrustedBySection />
       <BenefitsSection />
       <FeatureSection1 />
